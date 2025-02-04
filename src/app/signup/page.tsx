82949a74453f4a66ac2c74a2649514ebbc0b6527
Eduardo Pacheco <epacheco.dev@gmail.com>
2025-02-04 14:20:43 -0300

    create form signup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,5 +1,21 @@
-const SignUpPage = () => {
-  return <></>;
-};
+import styles from './styles.module.css';
+import IllustrationSignIn from '@/assets/images/illustration_signin.svg';
+import Image from 'next/image';
+import FormSignUp from '@/components/Forms/SignUp';
 
-export default SignUpPage;
+const SignInPage = () => (
+  <div className={styles.container}>
+    <div
+      className={styles.containerFormSignIn}
+      style={{ backgroundColor: '#d56c6c', width: '100%', height: '100%' }}
+    >
+      <Image width={700} src={IllustrationSignIn} alt="illustration" />
+    </div>
+    <div className={styles.containerFormSignIn}>
+      <h1>Obrigado por quere se juntar a nós!</h1>
+      <FormSignUp />
+    </div>
+  </div>
+);
+
+export default SignInPage;
