cc58fe2c3f939b6c1688c78b8b7bca7a9c9763cd
Eduardo Pacheco <epacheco.dev@gmail.com>
2025-02-04 13:38:14 -0300

    create form signin

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,5 +1,21 @@
+import styles from './styles.module.css';
 import FormSignIn from '@/components/Forms/SignIn';
+import IllustrationSignIn from '@/assets/images/illustration_signin.svg';
+import Image from 'next/image';
 
-const SignInPage = () => <FormSignIn />;
+const SignInPage = () => (
+  <div className={styles.container}>
+    <div className={styles.containerFormSignIn}>
+      <h1>Bem Vindo de Volta!</h1>
+      <FormSignIn />
+    </div>
+    <div
+      className={styles.containerFormSignIn}
+      style={{ backgroundColor: '#d56c6c', width: '100%', height: '100%' }}
+    >
+      <Image width={700} src={IllustrationSignIn} alt="illustration" />
+    </div>
+  </div>
+);
 
 export default SignInPage;
