cf9e4aee583ba8c0ba5e2492d29fb90f58fd281d
Eduardo Pacheco <epacheco.dev@gmail.com>
2025-02-06 19:01:42 -0300

    create component button default

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,13 @@
 import { InputHTMLAttributes } from 'react';
 import styles from './styles.module.css';
+import Default from '@/components/Button/Default';
 
 type Props = InputHTMLAttributes<HTMLInputElement>;
 
 const Button = (props: Props) => (
   <input className={styles.buttons} {...props} type={'submit'} />
 );
+
+Button.Default = Default;
 
 export default Button;
