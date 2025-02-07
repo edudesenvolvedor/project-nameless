501227dc2f69cdde8b681fa6234bc5633605f2d4
Eduardo Pacheco <epacheco.dev@gmail.com>
2025-02-07 08:41:50 -0300

    refactor dashboard component

---
+++ src/app/dashboard/service/page.tsx
@@ -0,0 +1,25 @@
+import Panel from '@/components/Panel';
+import Card from '@/components/Card';
+
+const Service = () => (
+  <>
+    <Panel
+      title={'Serviços'}
+      breadcrumb={'Dashboard / Serviços'}
+      header={<button>Novo Relatório</button>}
+    >
+      <Card.Project></Card.Project>
+      <Card.Project></Card.Project>
+      <Card.Project></Card.Project>
+      <Card.Project></Card.Project>
+      <Card.Project></Card.Project>
+      <Card.Project></Card.Project>
+      <Card.Project></Card.Project>
+      <Card.Project></Card.Project>
+      <Card.Project></Card.Project>
+      <Card.Project></Card.Project>
+    </Panel>
+  </>
+);
+
+export default Service;
