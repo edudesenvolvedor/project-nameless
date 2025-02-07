7a96cc88e0d17a43877356aa17f005480cfc71a0
Eduardo Pacheco <epacheco.dev@gmail.com>
2025-02-06 21:06:27 -0300

    create component panel

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,19 +1,26 @@
 import styles from './styles.module.css';
 import Item from '@/components/List/Reports/Item';
+import Panel from '@/components/Panel';
+import Card from '@/components/Card';
 
 const DashboardPage = () => (
-  <div className={styles.container}>
-    <div className={styles.header}>
-      <h1>Dashboard</h1>
-      <button>Novo Relatório</button>
+  <>
+    <Panel title={'Projetos'}>
+      <Card.Project></Card.Project>
+    </Panel>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <h1>Dashboard</h1>
+        <button>Novo Relatório</button>
+      </div>
+      <hr />
+      <div className={styles.list}>
+        <Item nameReport={'Relatório 1'} />
+        <Item nameReport={'Relatório 2'} />
+        <Item nameReport={'Relatório 3'} />
+      </div>
     </div>
-    <hr />
-    <div className={styles.list}>
-      <Item nameReport={'Relatório 1'} />
-      <Item nameReport={'Relatório 2'} />
-      <Item nameReport={'Relatório 3'} />
-    </div>
-  </div>
+  </>
 );
 
 export default DashboardPage;
