cf9db2e2b21bff919abe8f4120969dbd0ebd2b1f
Eduardo Pacheco <epacheco.dev@gmail.com>
2025-02-08 18:40:46 -0300

    refactor component FormSignUp

--- src/components/Forms/SignUp/index.tsx
+++ src/components/Forms/SignUp/index.tsx
@@ -1,5 +1,25 @@
-import styles from './styles.module.css';
+'use client';
 
-const FormSignUp = () => <form className={styles.container}></form>;
+import styles from './styles.module.css';
+import TextField from '@/components/Input/TextField';
+import Button from '@/components/Input/Button';
+import { redirect } from 'next/navigation';
+
+const FormSignUp = () => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    redirect('/auth/signin');
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.container}>
+      <TextField label={'Nome'} name={'name'} type={'text'} />
+      <TextField label={'Sobrenome'} name={'lastname'} type={'text'} />
+      <TextField label={'Email'} name={'email'} type={'email'} />
+      <TextField label={'Senha'} name={'password'} type={'password'} />
+      <Button label={'Criar Nova Conta'} />
+    </form>
+  );
+};
 
 export default FormSignUp;
