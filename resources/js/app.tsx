d5bfc75f709d9a2570a4b67857c027a6599160fb
Eduardo A. Pacheco <epacheco.dev@gmail.com>
2025-02-28 09:53:43 -0300

    add lib radix-ui

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,9 +1,11 @@
 import '../css/app.css';
 import './bootstrap';
+import "@radix-ui/themes/styles.css";
 
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
+import {Theme} from "@radix-ui/themes";
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -17,7 +19,11 @@ createInertiaApp({
     setup({ el, App, props }) {
         const root = createRoot(el);
 
-        root.render(<App {...props} />);
+        root.render(
+            <Theme>
+                <App {...props} />
+            </Theme>
+            );
     },
     progress: {
         color: '#4B5563',
